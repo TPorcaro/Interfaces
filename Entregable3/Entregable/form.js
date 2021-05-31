f58a0c60bde2795919a93cbda87cc0f9f3beb0ae
TomasPorcaro <tomasporcaro22@gmail.com>
2021-05-31 20:31:12 -0300

    submit button animation click

--- Entregable3/Entregable/form.js
+++ Entregable3/Entregable/form.js
@@ -5,7 +5,26 @@ document.addEventListener('DOMContentLoaded',() => {
         loader.classList.add('hide');
         body.classList.remove('hide');
     }, 3000);
+    const button   = document.querySelector('.submit-button'),
+      stateMsg = document.querySelector('.pre-state-msg');
+
+    const updateButtonMsg = function() {
+    button.classList.add('state-1', 'animated');
     
+    setTimeout(finalButtonMsg, 2000);
+    };
+
+    const finalButtonMsg = function() {
+    button.classList.add('state-2');
+    
+    setTimeout(setInitialButtonState, 2000);
+    };
+
+    const setInitialButtonState = function() {
+    button.classList.remove('state-1', 'state-2', 'animated');
+    };
+
+    button.addEventListener('click', updateButtonMsg);
     let linksNavBar = document.querySelectorAll('.navBarLink');
     linksNavBar.forEach(link => {
         link.addEventListener('click', () => {
